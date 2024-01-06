acfa170260f9b8e507731e4a351050c4a73f75d2
muhahmed2223 <muhahmed2223@gmail.com>
2024-01-06 21:43:30 +0500

    Update PlaceOrder.js

--- fast-cafe-system/src/Components/PlaceOrder.js
+++ fast-cafe-system/src/Components/PlaceOrder.js
@@ -2,18 +2,23 @@ import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import MenuContractData from "../Contracts/Menu_Management.json"
 import FastCoinsData from "../Contracts/FastCoin.json"
+import orderProcessingData from "../Contracts/OrderProcessing.json"
 import Addresses from "../address.json"
 
 export default function PlaceOrder() {
     const MenuABI = MenuContractData.abi;
     const FastCoinABI = FastCoinsData.abi;
+    const OrderProcessingABI = orderProcessingData.abi;
     const MenuContractAddress = Addresses.MenuContractAddress;
     const FastCoinContractAddress = Addresses.FastCoinContractAddress;
+    const OrderProcessingContractAddress = Addresses.OrderProcessingContractAddress;
     const ownerAddress = Addresses.Owner;
 
     const [menuNames, setMenuNames] = useState([]);
     const [menuPrices, setMenuPrices] = useState([]);
     const [menuAvailabilities, setMenuAvailabilities] = useState([]);
+
+    const [selectedRow, setSelectedRow] = useState(null);
 
     const [fastCoins, setFastCoins] = useState(0);
 
@@ -21,8 +26,49 @@ export default function PlaceOrder() {
 
     const [addCoins, setAddCoins] = useState(0);
 
+    const [itemQuantity, setItemQuantity] = useState(0);
+
+    const [msg, setMsg] = useState("");
+
     const updateAddCoins = (value) => {
         setAddCoins(value)
+    }
+
+    const handleRowClick = (index) => {
+        setSelectedRow(index === selectedRow ? null : index);
+    };
+
+    const itemQuantityChange = (event) => {
+        setItemQuantity(event.target.value);
+    }
+
+    const placeOrder = async () => {
+        try {
+            const web3 = new Web3('http://127.0.0.1:7545');
+            const OrderProcessingContract = new web3.eth.Contract(OrderProcessingABI, OrderProcessingContractAddress);
+            const FastCoinsContract = new web3.eth.Contract(FastCoinABI, FastCoinContractAddress);
+
+            const outputMsg = await OrderProcessingContract.methods.placeOrder(menuNames[selectedRow], itemQuantity).send({ from: userAddress, gas: 3000000 });
+            console.log(outputMsg);
+
+            const tempTokens = await FastCoinsContract.methods.balanceOf(userAddress).call();
+            const balanceAsNumber = Number(tempTokens);
+            setFastCoins(balanceAsNumber);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const transferCoins = async () => {
+        const web3 = new Web3('http://127.0.0.1:7545');
+        const FastCoinsContract = new web3.eth.Contract(FastCoinABI, FastCoinContractAddress);
+
+        await FastCoinsContract.methods.transfer(userAddress, addCoins).send({ from: ownerAddress });
+
+        const tempTokens = await FastCoinsContract.methods.balanceOf(userAddress).call();
+        const balanceAsNumber = Number(tempTokens);
+        setFastCoins(balanceAsNumber);
+
     }
 
     useEffect(() => {
@@ -36,8 +82,9 @@ export default function PlaceOrder() {
                 setUserAddress(tempUserAddress);
 
                 const tempTokens = await FastCoinsContract.methods.balanceOf(tempUserAddress).call();
-                setFastCoins(tempTokens);
-                console.log(tempTokens);
+                const balanceAsNumber = Number(tempTokens);
+                setFastCoins(balanceAsNumber);
+                console.log(balanceAsNumber);
 
                 const { 0: tempNames, 1: tempPrices, 2: tempAvailabilities } = await MenuContract.methods.displayMenu().call();
 
@@ -67,10 +114,37 @@ export default function PlaceOrder() {
                 <tbody>
                 </tbody>
             </table>
-            <div className="input-group mb-3">
-                <span className="input-group-text">Add Coins</span>
-                <input type="number" className="form-control" id="exampleInputPassword1" value={addCoins} onChange={(e)=>{updateAddCoins(e.target.value)}} />
+            <div className="input-group w-25">
+                <input type="number" className="form-control" id="exampleInputPassword1" value={addCoins} onChange={(e) => { updateAddCoins(e.target.value) }} aria-describedby="button-addon2" />
+                <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={transferCoins}>Add Coins</button>
             </div>
+            <h1>Menu</h1>
+            <table className='table table-hover'>
+                <thead>
+                    <tr>
+                        <th scope="col">Menu Item</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Availability</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {menuNames.map((name, index) => (
+                        menuAvailabilities[index] > 0 && (
+                            <tr key={index} className={index === selectedRow ? 'table-active' : ''} onClick={() => handleRowClick(index)}>
+                                <td>{name}</td>
+                                <td>{menuPrices[index]}</td>
+                                <td>{menuAvailabilities[index]}</td>
+                            </tr>
+                        )
+                    ))}
+                </tbody>
+            </table>
+            <div className="input-group mb-3">
+                <span className="input-group-text">Quantity</span>
+                <input type="number" className="form-control" id="exampleInputPassword1" value={itemQuantity} onChange={itemQuantityChange} aria-describedby="button-addon2" />
+                <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={placeOrder}>Place Order</button>
+            </div>
+            {msg}
         </div>
     )
 }
